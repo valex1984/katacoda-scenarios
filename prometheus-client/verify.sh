d8d35ad9639741e5401de9a684f51a2046dc1f28
Schetinnikov Stas <schetinnikov@gmail.com>
2020-12-23 09:38:58 +0300

    wip

--- prometheus-client/verify.sh
+++ prometheus-client/verify.sh
@@ -1,5 +1,6 @@
+echo "done"
+
 #!/bin/bash
-echo "OK"
 # TOKEN=`awk 'NR==1' /root/results.txt  | awk -F: '{print $2}' | sed -e 's/ //g'`
 # RPS=`awk 'NR==2' /root/results.txt  | awk -F: '{print $2}' | sed -e 's/ //g'`
 # LATENCY=`awk 'NR==3' /root/results.txt  | awk -F: '{print $2}' | sed -e 's/ //g'`
