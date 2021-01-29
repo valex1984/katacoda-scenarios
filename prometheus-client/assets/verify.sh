1f1519e828efa14046e1a1cb5fe598c8667c7a7a
Schetinnikov Stas <schetinnikov@gmail.com>
2021-01-29 17:38:53 +0300

    Fixed verify

--- prometheus-client/assets/verify.sh
+++ prometheus-client/assets/verify.sh
@@ -6,5 +6,5 @@ LATENCY=`awk 'NR==2' /root/results.txt  | awk -F: '{print $2}' | sed -e 's/ //g'
 if (( $RPS >= 6  &&  $RPS <= 8)) ; then RPS_OK=1; else RPS_OK=0; fi
 if (( $LATENCY >= 300  &&  $LATENCY <= 400)) ; then LATENCY_OK=1; else LATENCY_OK=0; fi
 
-[[ $RPS_OK == 1 && $LATENCY_OK == 1 ]] && echo '{"status": "ok"}'
-echo ""
+[[ $RPS_OK == 1 && $LATENCY_OK == 1 ]] && echo '{"allow":["OK"], "deny":[]}'
+echo '{"allow":[], "deny":["FAILED"]}'
