9bff56b330b8090f73381923906707b90174702c
Schetinnikov Stas <schetinnikov@gmail.com>
2021-03-21 17:38:39 +0300

    Rewrited verify process

--- prometheus-client/assets/validate.sh
+++ prometheus-client/assets/validate.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
 [ -f /root/results.txt ] || { echo "Создайте файл results.txt"; exit 1; }
 
-RPS=`awk 'NR==1' /root/results.txt  | awk -F: '{print $2}' | sed -e 's/ //g'`
-LATENCY=`awk 'NR==2' /root/results.txt  | awk -F: '{print $2}' | sed -e 's/ //g'`
-
-[ -z "$RPS" ] && echo "Введите не пустое значение количество запросов в секунду"
-[ -z "$LATENCY" ] && echo "Введите не пустое значение квантиля 0.9 для длительности запросов"
-
-if [ -z "$RPS" ] || [ -z "LATENCY" ]; then  exit 1 ; fi
+docker run -it --mount type=bind,source=/usr/local/bin/verify.py,target=/verify.py --mount type=bind,source=/root/results.txt,target=/results.txt python:3.7-slim python /verify.py --for-human
--- prometheus-client/assets/verify.sh
+++ prometheus-client/assets/verify.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-RPS=`awk 'NR==1' /root/results.txt  | awk -F: '{print $2}' | sed -e 's/ //g'`
-LATENCY=`awk 'NR==2' /root/results.txt  | awk -F: '{print $2}' | sed -e 's/ //g'`
-
-if (( $RPS >= 6  &&  $RPS <= 8)) ; then RPS_OK=1; else RPS_OK=0; fi
-if (( $LATENCY >= 300  &&  $LATENCY <= 400)) ; then LATENCY_OK=1; else LATENCY_OK=0; fi
-
-[[ $RPS_OK == 1 && $LATENCY_OK == 1 ]] && echo '{"allow":["OK"], "deny":[]}'
-[[ $RPS_OK == 1 && $LATENCY_OK == 1 ]] || echo '{"allow":[], "deny":["FAILED"]}'
+docker run -it --mount type=bind,source=/usr/local/bin/verify.py,target=/verify.py --mount type=bind,source=/root/results.txt,target=/results.txt python:3.7-slim python /verify.py --for-robot
