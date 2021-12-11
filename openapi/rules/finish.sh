446b9417635d7adaadda1f5a586710f23702064f
Alexander Vasilev <avasilev@integrity-solutions.ru>
2021-12-11 14:23:46 +0300

    fix opa script

--- openapi/rules/finish.sh
+++ openapi/rules/finish.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-[[ -f /root/envs ]] && source /root/envs || \
-    { jq --null-input --arg error "envs not exists, environment fail" '{"error": $error }' && exit 1 }
+[[ ! -f /root/envs ]] && jq --null-input --arg error "envs not exists, environment fail" '{"error": $error }' && exit 1 || source /root/envs
 
-faas_fn1_out=$(curl $OPENFAAS_URL/function/fn1)
-gravitee_fn1_out=$(curl http://localhost:32100/gateway/serverless)
-gravitee_fn2_out=$(curl -H @/root/apikey http://localhost:32100/gateway/fn2)
+faas_fn1_out="$(curl -s $OPENFAAS_URL/function/fn1)"
+gravitee_fn1_out="$(curl -s http://localhost:32100/gateway/serverless)"
+gravitee_fn2_out="$(curl -s -H @/root/apikey http://localhost:32100/gateway/fn2)"
 
 jq --null-input \
 --arg faas_fn1_out "$faas_fn1_out" \
