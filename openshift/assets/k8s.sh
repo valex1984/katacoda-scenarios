3b2bd09ab5a5f8e1ced32af0bbc5bebebe584abe
Alexander Vasiliev <avasilev@integrity-solutions.ru>
2022-09-18 18:11:45 +0300

    add kafka autodeploy

--- openshift/assets/k8s.sh
+++ openshift/assets/k8s.sh
@@ -9,14 +9,21 @@ infra_project=infra
 work_project=work
 infra_context=${infra_project}-${_user}
 work_context=${work_project}-${_user}
-oc config set-context ${infra_context}
+oc config use-context ${infra_context}
 curr_project=$(oc project -q)
 
+####deploy postgres
 pgpass=$(cat /proc/sys/kernel/random/uuid)
 oc create secret generic pg-postgresql --from-literal=postgres-password="$pgpass" --dry-run=client -oyaml \
 | kubectl apply -f-
 oc apply -f /usr/local/k8s -n "${curr_project}"
 
-echo "postgres: pg.${infra_project}.svc.cluster.local:5432   user: postgres   password: $pgpass" > $OUT_FILE
+echo -e "postgres:\n  addr: pg.${curr_project}.svc.cluster.local:5432\n  user: postgres\n  password: $pgpass" > $OUT_FILE
+####end
 
-oc config set-context ${work_context}
+####deploy kafka
+sed "s/PROJECT_PLACEHOLDER/${curr_project}/g" /usr/local/k8s_tpl/kafka_tpl.yaml |oc apply -f-
+echo -e "kafka:\n  addr: kafka.${curr_project}.svc.cluster.local:9092" >> $OUT_FILE
+####end
+
+oc config use-context ${work_context}
