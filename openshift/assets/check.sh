3669d99ebfa927ddda8afe6a2797dbdbab9e770f
Alexander Vasiliev <avasilev@integrity-solutions.ru>
2022-09-19 17:22:26 +0300

    refactor check script

--- openshift/assets/check.sh
+++ openshift/assets/check.sh
@@ -8,7 +8,6 @@ infra_context=${infra_project}-${_user}
 work_context=${work_project}-${_user}
 
 oc config use-context ${infra_context} > /dev/null
-curr_project=$(oc project -q)
 
 spinner() {
     local i sp n
@@ -22,15 +21,9 @@ spinner() {
 
 spinner &
 
-echo "[INFO] waiting for starting infra pods..."
-while true
-    do 
-    count=$(oc get po --ignore-not-found|wc -l)
-    [[ $count -gt 0 ]] && break
-    sleep 2
-done
-echo "[INFO] done"
 echo "[INFO] waiting for infra pods is ready.."
+count=$(oc get po --ignore-not-found|wc -l)
+[[ $count -eq 0 ]] && sleep 10 #wait for pods scheduled
 oc wait --for=condition=ready pod  --all --timeout=3m  >/dev/null 2>&1
 test $? -eq 1 && echo "[ERROR] infra pods not ready" && kill "$!" && exit 1
 
