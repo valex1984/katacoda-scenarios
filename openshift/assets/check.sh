e6a0e3e212ed55b88955355bae238fd5ece7518e
Alexander Vasiliev <avasilev@integrity-solutions.ru>
2022-09-19 18:21:37 +0300

    test check script

--- openshift/assets/check.sh
+++ openshift/assets/check.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 _user=student
 infra_project=infra
@@ -32,8 +32,7 @@ echo "[INFO] waiting for infra pods is ready.."
 while true;
     do
     count=$(oc get po --ignore-not-found|wc -l)
-    [[ $count -gt 0 ]] && break 
-    sleep 2
+    [[ $count -eq 0 ]] && sleep 2 || break 
 done
 oc wait --for=condition=ready pod  --all --timeout=3m  #>/dev/null 2>&1
 test $? -eq 1 && echo "[ERROR] infra pods not ready" && kill "$!" && exit 1
