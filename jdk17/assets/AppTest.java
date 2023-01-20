b3dc4daef883c94d3b31696f5cfca23531519940
Alexander Vasiliev <avasilev@integrity-solutions.ru>
2023-01-20 08:58:40 +0300

    change jdk17

---
+++ jdk17/assets/AppTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    void assertjShouldWork() {
+        assertThat(true).isTrue();
+    }
+}
