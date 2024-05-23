4be8c8250a13de53f9d2cbaca8cf4f1f1d5c0920
Alexander Vasiliev <avasilev@integrity-solutions.ru>
2024-05-23 15:02:14 +0300

    fix python script

--- python/assets/venv.sh
+++ python/assets/venv.sh
@@ -7,9 +7,9 @@ DONE_FILE=/usr/local/etc/venv.done
 
 [ -f $DONE_FILE ] && exit 0
 
-python3 -m venv $venv_name && source $venv_name/bin/activate
+python3 -m venv $venv_name && source ${venv_name}/bin/activate
 
-echo "source $venv_name/bin/activate" >  ~/.bashrc
+echo "source $venv_name/bin/activate" >>  ~/.bashrc
 
 touch $DONE_FILE
 
